c025c2d43a6d5a26ca13f7cf429af5ea60cdbee1
Justin Le <mstksg@gmail.com>
2014-01-18 21:55:51 -0800

    setting things up to be ready for sdl-ing

--- src/Experiment/Planets.hs
+++ src/Experiment/Planets.hs
@@ -67,7 +67,6 @@ runFourBody :: (Body,V3D) -> (Body,V3D) -> (Body,V3D) -> (Body,V3D) -> IO ()
 runFourBody b1 b2 b3 b4 =
   runTest 4 $ arr (\(a,b,c,d) -> [a,b,c,d]) . fourBody b1 b2 b3 b4 verlet
 
-
 runTest :: Int -> Wire (Timed Double ()) String IO () [Body] -> IO ()
 runTest n w = do
     clearLogs 10
@@ -84,63 +83,4 @@ writeLog n bodies = forM_ [0..(n-1)] $ \i ->
   appendFile
     ("out/planets_b" ++ show i ++ ".dat")
     ((++ "\n") . gnuplot $ bodies !! i)
---     (unlines . map (gnuplot . (!! i)) $ logs)
-
--- writeLogs :: [[Body]] -> Int -> IO ()
--- writeLogs logs n = forM_ [0..(n-1)] $ \i ->
---   writeFile
---     ("out/planets_b" ++ show i ++ ".dat")
---     (unlines . map (gnuplot . (!! i)) $ logs)
-
-
-
-  -- writeFile "out/planets_b0.dat" . unlines . map (gnuplot . (!! 0)) $ logs
-  -- writeFile "out/planets_b1.dat" . unlines . map (gnuplot . (!! 1)) $ logs
-  -- writeFile "out/planets_b2.dat" . unlines . map (gnuplot . (!! 2)) $ logs
-  -- where
-  --   logs = execWriter logWriter
-  --   logWriter = testWireRight
-  --     50000
-  --     (0.02 :: Double)
-  --     (tell . return)
-  --     (threeBody verlet :: (MonadFix m, HasTime t s) => Wire s String m () [Body])
-
-    -- writeFile "out/planets_newton.dat" $ unlines (logs euler)
-    -- writeFile "out/planets_verlet.dat" $ unlines (logs verlet)
-  -- where
-    -- logs i = execWriter $ logWriter i
-    -- logWriter i = testWireRight
-    --   10000
-    --   (0.01 :: Double)
-    --   (tell . return)
-    --   (gnuplot <$> body x0 v0 xa i
-    --       :: (HasTime t s, MonadFix m)
-    --           => Wire s String m () String)
-    -- x0 = zero
-    -- v0 = V3 1   0 0.05
-    -- xa = V3 0.2 1 0
-
--- main :: IO ()
--- main = SDL.withInit [SDL.InitEverything] $ do
---   screen <- SDL.setVideoMode 600 600 32 [SDL.SWSurface]
---   -- void $ go undefined screen clockSession planets
---   return ()
-
- -- where
- --   go = undefined
-
-  -- go keysDown screen s w = do
-  --   keysDown' <- parseEvents keysDown
-  --   (x, w', s') <- stepSession_ w s keysDown'
-
-  --   (SDL.mapRGB . SDL.surfaceGetPixelFormat) screen 255 255 255 >>=
-  --       SDL.fillRect screen Nothing
-
-  --   (SDL.mapRGB . SDL.surfaceGetPixelFormat) screen 0 50 200 >>=
-  --       SDL.fillRect screen (Just $ SDL.Rect (round x) 0 50 50)
-
-  --   SDL.flip screen
-  --   go keysDown' screen s' w'
-
-
 
--- src/Render/Backend/SDL.hs
+++ src/Render/Backend/SDL.hs
@@ -4,12 +4,14 @@ import Control.Wire
 import Render.Render
 import qualified Graphics.UI.SDL           as SDL
 import qualified Graphics.UI.SDL.Framerate as Framerate
+import Data.Word
 
 sdlBackend ::
        Int
     -> Int
+    -> (Word8, Word8, Word8)
     -> Backend (Timed NominalDiffTime ()) e IO (SDL.Surface -> IO ()) a
-sdlBackend ht wd = Backend runGnuplot
+sdlBackend ht wd (cr,cg,cb) = Backend runGnuplot
   where
     runGnuplot r wr = SDL.withInit [SDL.InitEverything] $ do
       screen <- SDL.setVideoMode ht wd 0 [SDL.SWSurface]--, SDL.Fullscreen]
@@ -28,8 +30,19 @@ sdlBackend ht wd = Backend runGnuplot
 
           case mx of
             Right mx' -> do
+
               Framerate.delay frameRate
+
+              pix <- SDL.mapRGB (SDL.surfaceGetPixelFormat screen) cr cg cb
+              SDL.fillRect screen Nothing pix
+
               r mx' screen
+
               go screen s w frameRate
 
             Left _ -> return ()
+
+
+class SDLRenderable a where
+  renderSDL :: a -> SDL.Surface -> IO ()
+
