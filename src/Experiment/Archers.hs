c60c6798ef06241dbeb5b6dd746844187634b464
Justin Le <mstksg@gmail.com>
2014-01-29 20:03:52 -0800

    both can now collide and stuff

--- src/Experiment/Archers.hs
+++ src/Experiment/Archers.hs
@@ -4,12 +4,14 @@ module Main where
 
 -- import Control.Arrow
 import Control.Category
+import Control.Monad
 import Control.Monad.Fix
 import Control.Wire              as W
+-- import Control.Wire.Unsafe.Event
+import Data.Maybe                (maybeToList)
 import FRP.Netwire
 import Linear.Metric
 import Linear.V2
-import Control.Monad
 import Linear.V3
 import Linear.Vector
 import Physics
@@ -72,9 +74,11 @@ main = testStage (simpleStage 400 300)
 simpleStage :: (MonadFix m, HasTime t s, Monoid e, Fractional t) => Double -> Double -> Wire s e m () Stage
 simpleStage w h = proc _ -> do
     rec
-      (dart',hit) <- dartH x0 v0 . delay (Archer (Body 1 zero) 0 ) -< head archers
+      (dart',hit) <- dartHM x0 v0 . delay (Archer (Body 1 zero) 0) -< head archers
+      let
+        darts = maybeToList dart'
       archers <- arr return . archer a0 --> pure [] -< hit
-    returnA -< Stage w h archers [dart']
+    returnA -< Stage w h archers darts
   where
     x0 = V3 w (h/2) 0
     v0 = V3 (-12) 0 0
@@ -104,6 +108,33 @@ dartH x0 v0@(V3 vx vy _) = dart'
           -< (arcpos ^-^ pos, Hit)
       returnA -< (Dart (Body 1 pos) (atan2 vy vx),hitArcher)
 
+dartHM :: forall s t e m. (Monad m, HasTime t s, Monoid e) => V3D -> V3D -> Wire s e m Archer (Maybe Dart, Event Hit)
+dartHM x0 v0@(V3 vx vy _) = dart'
+  where
+    dart' :: Wire s e m Archer (Maybe Dart, Event Hit)
+    dart' = proc (Archer (Body _ arcpos) _) -> do
+      pos <- integral x0 -< v0
+      hitArcher <-
+        never . W.unless (\(d,_) -> norm d < 5) --> arr (snd<$>) . now
+          -< (arcpos ^-^ pos, Hit)
+      let
+        d = Dart (Body 1 pos) (atan2 vy vx)
+      dartStill <- Nothing <$ hold <|> pure (Just ()) -< hitArcher
+      let
+        dart'' = d <$ dartStill
+      returnA -< (dart'',hitArcher)
+
+dartHH :: forall s t e m. (Monad m, HasTime t s, Monoid e) => V3D -> V3D -> Wire s e m (Archer, Event Hit) (Dart, Event Hit)
+dartHH x0 v0@(V3 vx vy _) = hittable dart'
+  where
+    dart' :: Wire s e m Archer (Dart, Event Hit)
+    dart' = proc (Archer (Body _ arcpos) _) -> do
+      pos <- integral x0 -< v0
+      hitArcher <-
+        never . W.unless (\(d,_) -> norm d < 5) --> arr (snd<$>) . now
+          -< (arcpos ^-^ pos, Hit)
+      returnA -< (Dart (Body 1 pos) (atan2 vy vx),hitArcher)
+
 archer :: forall s t e m. (Monad m, Monoid e, HasTime t s, Fractional t) => V3D -> Wire s e m (Event Hit) Archer
 archer x0 = hittable archer' . arr ((),)
   where
@@ -115,6 +146,16 @@ archer x0 = hittable archer' . arr ((),)
       (pos, vx, vy) <- returnA -< (x0,0,0)
       returnA -< Archer (Body 1 pos) (atan2 vy vx)
 
+-- archerH :: forall s t e m. (Monad m, Monoid e, HasTime t s, Fractional t) => V3D -> Wire s e m (Event Hit) Archer
+-- archerH x0 = hittable archer' . arr ((),)
+--   where
+--     archer' :: Wire s e m () Archer
+--     archer' = proc _ -> do
+--       -- vx <- hold . stdNoiseR 2 (-10,10) 1 -< ()
+--       -- vy <- hold . stdNoiseR 3 (-10,10) 2 -< ()
+--       -- pos <- integral x0 -< V3 vx vy 0
+--       (pos, vx, vy) <- returnA -< (x0,0,0)
+--       returnA -< Archer (Body 1 pos) (atan2 vy vx)
 
 hittable :: (Monad m, Monoid e) => Wire s e m a b -> Wire s e m (a, Event Hit) b
 hittable wr = proc (a,h) -> do
