99987fbe615d7eb4ee7dcbce6273bc526ffc7524
Justin Le <mstksg@gmail.com>
2014-01-30 19:08:29 -0800

    zooming?

--- src/Experiment/Planets.hs
+++ src/Experiment/Planets.hs
@@ -5,6 +5,7 @@ module Main where
 import Control.Category
 import Control.Monad.Writer.Strict
 import Control.Wire                         as W
+import Data.Maybe                           (fromJust, isJust)
 import Data.Traversable
 import Experiment.Planets.Instances.GNUPlot ()
 import Experiment.Planets.Types
@@ -56,20 +57,29 @@ main = do
   mapM_ print pData
   runManyMouse pData
 
-runMany :: [(Planet, V3D)] -> IO ()
-runMany planets = runTest (length planets) (w . pure ())
-  where
-    bwire = manyBody (map bTup planets) euler
-    w = arr (zipWith ($) planetMakers) . bwire
-    planetMakers = map (pMaker . fst) planets
+-- runMany :: [(Planet, V3D)] -> IO ()
+-- runMany planets = runTest (length planets) (w . pure ())
+--   where
+--     bwire = manyBody (map bTup planets) euler
+--     w = arr (zipWith ($) planetMakers) . bwire
+--     planetMakers = map (pMaker . fst) planets
 
 runManyMouse :: [(Planet, V3D)] -> IO ()
-runManyMouse planets = runTest (length planets + 1) w
+runManyMouse planets = runTest (length planets + 1) wz
   where
     bwire = manyBody' (map bTup planets) verlet
     w = zipWith ($) planetMakers <$> bwire
+    wz = proc e -> do
+      bs <- w -< e
+      zoomE <- filterE isJust -< zoomEvent <$> e
+      zoom <- hold . accumE (*) 1 <|> pure 1 -< fromJust <$> zoomE
+      returnA -< (zoom,bs)
     planetMakers = map (pMaker . fst) (planets ++ [extraPlanet])
     extraPlanet = (Planet "Bob" 0.1 (255,255,255) (Body 0 zero), zero)
+
+    zoomEvent (RenderMouseDown _ RenderMouseWheelUp) = Just (1/1.2)
+    zoomEvent (RenderMouseDown _ RenderMouseWheelDown) = Just (1.2)
+    zoomEvent _ = Nothing
 
 manyBody' :: forall m e t s. (MonadFix m, Monoid e, HasTime t s, Fractional t)
     => [(Body, V3D)]          -- Initial body states and initial velocities
@@ -97,32 +107,32 @@ manyBody' bodyList igr = proc e -> do
     emptyMouse = RenderMouseUp (0,0) RenderMouseLeft
 
 
-runFixed :: [(Planet, V3D)] -> IO ()
-runFixed pData = runTest 9 (w . pure ())
-  where
-    bwire = manyFixedBody [sun] (map bTup pData) verlet
-    w = zipWith ($) planetMakers <$> bwire
-    ((Planet _ _ _ sun,_):planets) = pData
-    planetMakers = map (pMaker . fst) planets
+-- runFixed :: [(Planet, V3D)] -> IO ()
+-- runFixed pData = runTest 9 (w . pure ())
+--   where
+--     bwire = manyFixedBody [sun] (map bTup pData) verlet
+--     w = zipWith ($) planetMakers <$> bwire
+--     ((Planet _ _ _ sun,_):planets) = pData
+--     planetMakers = map (pMaker . fst) planets
 
-runTwoBody :: (Planet,V3D) -> (Planet, V3D) -> IO ()
-runTwoBody (p1@(Planet _ _ _ b1), v1) (p2@(Planet _ _ _ b2), v2) =
-    runTest 2 (w . pure ())
-  where
-    w = planetMaker <$> twoBody (b1,v1) (b2,v2) verlet
-    planetMaker (a,b) = [pMaker p1 a, pMaker p2 b]
+-- runTwoBody :: (Planet,V3D) -> (Planet, V3D) -> IO ()
+-- runTwoBody (p1@(Planet _ _ _ b1), v1) (p2@(Planet _ _ _ b2), v2) =
+--     runTest 2 (w . pure ())
+--   where
+--     w = planetMaker <$> twoBody (b1,v1) (b2,v2) verlet
+--     planetMaker (a,b) = [pMaker p1 a, pMaker p2 b]
 
-runOneBody :: (Planet,V3D) -> IO ()
-runOneBody (p@(Planet _ _ _ b0), v0) = runTest 1 (w . pure ())
-  where
-    w = map (pMaker p) <$> manyFixedBody [Body 1 zero] [(b0,v0)] verlet
+-- runOneBody :: (Planet,V3D) -> IO ()
+-- runOneBody (p@(Planet _ _ _ b0), v0) = runTest 1 (w . pure ())
+--   where
+--     w = map (pMaker p) <$> manyFixedBody [Body 1 zero] [(b0,v0)] verlet
 
-runTest :: Int -> Wire (Timed Double ()) () IO (Event RenderEvent) [Planet] -> IO ()
+runTest :: Int -> Wire (Timed Double ()) () IO (Event RenderEvent) (Double, [Planet]) -> IO ()
 runTest _ w =
 #ifdef WINDOWS
-  runBackend (glutBackend 600 600 (31,31,31)) (const . return $ ()) (PlanetList <$> w)
+  runBackend (glutBackend 600 600 (31,31,31)) (const . return $ ()) (uncurry PlanetList <$> w)
 #else
-  runBackend (sdlBackend 600 600 (31,31,31)) (const . return . return $ ()) (PlanetList <$> w)
+  runBackend (sdlBackend 600 600 (31,31,31)) (const . return . return $ ()) (uncurry PlanetList <$> w)
 #endif
 
 runTestGNUPlot :: Int -> Wire (Timed Double ()) () IO (Event RenderEvent) [Planet] -> IO ()
