58ecaf6eb30fbc9efbbb63da7ac3c134a163f402
Justin Le <mstksg@gmail.com>
2014-02-17 02:19:08 -0800

    preliminary types for battlefield

--- src/Experiment/Battlefield/Types.hs
+++ src/Experiment/Battlefield/Types.hs
@@ -1,11 +1,51 @@
 module Experiment.Battlefield.Types where
 
-import Render.Surface
+import Render.Sprite
+import Control.Wire
+import Linear.V3
 
 data Stage = Stage { stageWidth    :: Double
                    , stageHeight   :: Double
-                   , stageEntities :: [Entity]
-                   }
+                   , stageSoldiers :: [Soldier]
+                   , stageArticles :: [Article]
+                   } deriving Show
 
-data Soldier = Soldier
+data Soldier = Soldier  { soldierPosAng :: PosAng
+                        , soldierHealth :: Double
+                        , soldierFlag   :: Maybe TeamFlag
+                        , soldierBody   :: SoldierBody
+                        , soldierWeapon :: SoldierWeapon
+                        , soldierMount  :: SoldierMount
+                        } deriving Show
+
+data SoldierBody = MeleeBody | TankBody | RangedBody
+                     deriving Show
+
+data SoldierWeapon = Sword | Axe | Bow | Longbow
+                       deriving Show
+
+data SoldierMount = Foot | Horse
+                      deriving Show
+
+data TeamFlag = TeamFlag  { teamFlagColor :: Color
+                          } deriving Show
+
+data Article = Article { articlePosAng :: PosAng
+                       , articleType   :: ArticleType
+                       } deriving Show
+
+data ArticleType = Dart deriving Show
+
+data PosAng = PosAng { posAngPos :: V3 Double
+                     , posAngAng :: Double
+                     } deriving Show
+
+data SoldierOutEvent = MeleeEvent | DartEvent
+
+type SoldierOutEvents = Event [SoldierOutEvent]
+
+data SoldierInEvent = HitEvent
+
+type SoldierInEvents = Event [SoldierInEvent]
+
 
