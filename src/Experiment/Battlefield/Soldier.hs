76cf523fc5e657cc64ed5287cde2f48c92f462dc
Justin Le <mstksg@gmail.com>
2014-02-19 13:27:11 -0800

    balancing

--- src/Experiment/Battlefield/Soldier.hs
+++ src/Experiment/Battlefield/Soldier.hs
@@ -47,7 +47,7 @@ soldierWire (SoldierData x0 fl bod weap mnt gen) =
     -- shoot!
     shot  <- shoot -< newD
     -- this is bad frp but :|
-    shotR <- couple (noisePrimR (0.833,1.2) gen) -< shot
+    shotR <- couple (noisePrimR (1/damageVariance,damageVariance) gen) -< shot
     let
       shot' = (\(atk,r) -> [atk (r * baseDamage)]) <$> shotR
 
@@ -83,7 +83,7 @@ soldierWire (SoldierData x0 fl bod weap mnt gen) =
     startingHealth = bodyHealth bod * mountHealthMod mnt
     speed = mountSpeed mnt * bodySpeedMod bod
     recovery = startingHealth / recoveryFactor
-    baseDamage = weaponDamage weap * mountDamageMod mnt
+    baseDamage = weaponDPS weap * (realToFrac coolDownTime) * mountDamageMod mnt
     coolDownTime = weaponCooldown weap
     findClosest :: [V3 Double] -> V3 Double -> Maybe ((Double, V3 Double),V3 Double)
     findClosest [] _       = Nothing
@@ -113,7 +113,7 @@ soldierWire (SoldierData x0 fl bod weap mnt gen) =
 
 bodyHealth :: SoldierBody -> Double
 bodyHealth MeleeBody  = 25
-bodyHealth TankBody   = 60
+bodyHealth TankBody   = 55
 bodyHealth RangedBody = 12
 
 mountHealthMod :: Mount -> Double
@@ -129,15 +129,18 @@ bodySpeedMod MeleeBody  = 1
 bodySpeedMod TankBody   = 0.5
 bodySpeedMod RangedBody = 1
 
-weaponDamage :: Weapon -> Double
-weaponDamage Sword   = 4
-weaponDamage Axe     = 12
-weaponDamage Bow     = 2.5
-weaponDamage Longbow = 2.5
+weaponDPS :: Weapon -> Double
+weaponDPS Sword   = 8.0
+weaponDPS Axe     = 10.0
+weaponDPS Bow     = 2.75
+weaponDPS Longbow = 2.5
 
 mountDamageMod :: Mount -> Double
 mountDamageMod Foot  = 1
 mountDamageMod Horse = 1.25
+
+damageVariance :: Double
+damageVariance = 1.15
 
 weaponRange :: Weapon -> Double
 weaponRange Sword   = 7.5
@@ -152,4 +155,4 @@ weaponCooldown Bow     = 1
 weaponCooldown Longbow = 1.5
 
 recoveryFactor :: Double
-recoveryFactor = 18
+recoveryFactor = 21
